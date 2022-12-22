2e61ceeaf2cf52ad95d784c058ad998c47c3f4da
wojtvss <wojtidomino@gmail.com>
2022-12-22 11:03:52 +0100

    polymorphism
    I wrote a code that helps understanding polymorphism definition in C++.

---
+++ kodzik1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Drink {
+    protected: 
+        int price;
+    public:
+        void setPrice(int a){
+            price = a;
+        }
+};
+
+class Coffee : public Drink{
+    public:
+        void make() {
+            cout << "Coffee: "<<price<<endl;
+        }
+};
+
+class Tea : public Drink {
+    public:
+        void make() {
+            cout << "Tea: "<<price<<endl;
+        }
+};
+
+int main() {
+    Coffee c;
+    Tea t;
+    Drink *e1 = &c;
+    Drink *e2 = &t;
+
+    e1->setPrice(5);
+    e2->setPrice(6);
+
+    c.make();
+    t.make();
+}
